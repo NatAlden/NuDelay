5b8b3caaa2c25dc15aeb64187b80090a9904c8d2
ShamsAstro <moslemhassiki4@gmail.com>
2025-08-07 17:05:51 -0500

    Start new simulation code to verify PATT results

---
+++ Trigger_simulation_and_tests/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import sys
+import math
+import time
+import random
+import json
+from pathlib import Path
+
+lis=np.linspace(10, 55,7 )
+
+print(np.round(lis)[[2, 4, -1]])
+print(np.round(lis[[2, 4, 5]]))
